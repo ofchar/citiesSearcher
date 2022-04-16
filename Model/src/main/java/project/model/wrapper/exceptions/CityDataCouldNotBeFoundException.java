ef386aed5ad9f0ee4d56f5293b4f6115d01cbf07
ofchar <matowczarek@outlook.com>
2022-04-16 13:40:12 +0100

    Added custom exceptions

---
+++ Model/src/main/java/project/model/wrapper/exceptions/CityDataCouldNotBeFoundException.java
@@ -0,0 +1,19 @@
+package project.model.wrapper.exceptions;
+
+public class CityDataCouldNotBeFoundException extends Exception {
+
+    public CityDataCouldNotBeFoundException() {
+    }
+
+    public CityDataCouldNotBeFoundException(String message) {
+        super(message);
+    }
+
+    public CityDataCouldNotBeFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CityDataCouldNotBeFoundException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ Model/src/main/java/project/model/wrapper/exceptions/WikipediaWrapperException.java
@@ -0,0 +1,19 @@
+package project.model.wrapper.exceptions;
+
+public class WikipediaWrapperException extends Exception {
+
+    public WikipediaWrapperException() {
+    }
+
+    public WikipediaWrapperException(String message) {
+        super(message);
+    }
+
+    public WikipediaWrapperException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public WikipediaWrapperException(Throwable cause) {
+        super(cause);
+    }
+}
