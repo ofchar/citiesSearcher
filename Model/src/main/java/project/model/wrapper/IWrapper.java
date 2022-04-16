29ec3ec89f6c94678981b53c7a15412da85c6200
ofchar <matowczarek@outlook.com>
2022-04-16 13:39:54 +0100

    Added throws clauses to Wrapper interface

--- Model/src/main/java/project/model/wrapper/IWrapper.java
+++ Model/src/main/java/project/model/wrapper/IWrapper.java
@@ -10,141 +10,179 @@ public interface IWrapper {
     
     /**
      * Finds and returns the name of the city.
+     * Throws Exception if data could not be found.
      * 
      * @return String
+     * @throws java.lang.Exception
      */
-    public String getName();
+    public String getName() throws Exception;
     
     /**
      * Finds and returns the name of the country,
      * to which the city belongs.
+     * Throws Exception if data could not be found.
      * 
      * @return String
+     * @throws java.lang.Exception
      */
-    public String getCountry();
+    public String getCountry() throws Exception;
     
     /**
      * Finds and returns boolean that represents
      * if the city is a capital of a country.
+     * Throws Exception if data could not be found.
      * 
      * @return boolean
+     * @throws java.lang.Exception
      */
-    public boolean isCapital();
+    public boolean isCapital() throws Exception;
     
     /**
      * Finds and returns the URL to the image with
      * flag of the country to which the city belongs.
+     * Throws Exception if data could not be found.
      * 
      * @return String
+     * @throws java.lang.Exception
      */
-    public String getCountryFlag();
+    public String getCountryFlag() throws Exception;
     
     /**
      * Finds and returns List of Strings representing every
      * official language spoken in the country to which 
      * the city belongs.
+     * Throws Exception if data could not be found.
      * 
      * @return List
+     * @throws java.lang.Exception
      */
-    public List<String> getCountryLanguages();
+    public List<String> getCountryLanguages() throws Exception;
     
     /**
      * Finds and returns the URL to the image with
      * flag of the city.
+     * Throws Exception if data could not be found.
      * 
      * @return String
+     * @throws java.lang.Exception
      */
-    public String getCityFlag();
+    public String getCityFlag() throws Exception;
     
     /**
      * Finds and returns List of urls to images of City's landmarks.
+     * Throws Exception if data could not be found.
      * 
      * @return List
+     * @throws java.lang.Exception
      */
-    public List<String> getCityLandmarks();
+    public List<String> getCityLandmarks() throws Exception;
     
     /**
      * Finds and returns area of the city in km^2.
+     * Throws Exception if data could not be found.
      * 
      * @return float
+     * @throws java.lang.Exception
      */
-    public float getArea();
+    public float getArea() throws Exception;
 
     /**
      * Finds and returns number of inhabitants of the city.
+     * Throws Exception if data could not be found.
      * 
      * @return int
+     * @throws java.lang.Exception
      */
-    public int getInhabitants();
+    public int getInhabitants() throws Exception;
 
     /**
      * Finds and returns city's population density in
      * number of inhabitants per square kilometer.
+     * Throws Exception if data could not be found.
      * 
      * @return float
+     * @throws java.lang.Exception
      */
-    public float getPopulationDensity();
+    public float getPopulationDensity() throws Exception;
     
     /**
      * Finds and returns city's postal code.
+     * Throws Exception if data could not be found.
      * 
      * @return String
+     * @throws java.lang.Exception
      */
-    public String getPostalCode();
+    public String getPostalCode() throws Exception;
       
     /**
      * Finds and returns name and surname of city's mayor.
+     * Throws Exception if data could not be found.
      * 
      * @return String
+     * @throws java.lang.Exception
      */
-    public String getMayorName();
+    public String getMayorName() throws Exception;
     
     /**
      * Finds and returns city's latitude.
+     * Throws Exception if data could not be found.
      * 
      * @return float
+     * @throws java.lang.Exception
      */
-    public float getLatitude();
+    public float getLatitude() throws Exception;
     
     /**
      * Finds and returns city's longitude.
+     * Throws Exception if data could not be found.
      * 
      * @return float
+     * @throws java.lang.Exception
      */
-    public float getLongitude();
+    public float getLongitude() throws Exception;
     
     /**
      * Finds and returns city's altitude (in meters).
+     * Throws Exception if data could not be found.
      * 
      * @return float
+     * @throws java.lang.Exception
      */
-    public float getAltitude();
+    public float getAltitude() throws Exception;
     
     /**
      * Finds and returns city's climate information.
+     * Throws Exception if data could not be found.
      * 
      * @return String
+     * @throws java.lang.Exception
      */
-    public String getClimate();
+    public String getClimate() throws Exception;
     
     /**
      * Finds and returns city's timezone.
+     * Throws Exception if data could not be found.
      * 
      * @return String
+     * @throws java.lang.Exception
      */
-    public String getTimezone();
+    public String getTimezone() throws Exception;
     
     /**
      * Finds and returns city's website.
+     * Throws Exception if data could not be found.
      * 
      * @return String
+     * @throws java.lang.Exception
      */
-    public String getWebsite();
+    public String getWebsite() throws Exception;
     
     /**
      * Finds and returns List of city's twin towns.
+     * Throws Exception if data could not be found.
      * 
      * @return List
+     * @throws java.lang.Exception
      */
-    public List<String> getTwinTowns();
+    public List<String> getTwinTowns() throws Exception;
 }
