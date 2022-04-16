f7066c69330bc27f8593bc5e4dbd8a8f3cba3075
ofchar <matowczarek@outlook.com>
2022-04-16 13:41:32 +0100

    simple test in main

--- Model/src/main/java/project/model/Main.java
+++ Model/src/main/java/project/model/Main.java
@@ -1,8 +1,20 @@
 package project.model;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import project.model.wrapper.CityDataHelper;
+import project.model.wrapper.exceptions.CityDataCouldNotBeFoundException;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        CityDataHelper helper = new CityDataHelper("oporto");
+        
+        try {
+            System.out.println(helper.getInhabitants());
+        } catch (CityDataCouldNotBeFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
