4d0a8be1a670f6a253e0c480b80f70c56025fbe1
ofchar <matowczarek@outlook.com>
2022-04-25 20:57:21 +0100

    Some beginings of crud alike methosd in XMLHelper

--- Model/src/main/java/project/model/xmler/XMLHelper.java
+++ Model/src/main/java/project/model/xmler/XMLHelper.java
@@ -1,8 +1,10 @@
 package project.model.xmler;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import project.model.xmler.exceptions.XMLFileHelperException;
@@ -55,6 +57,10 @@ public class XMLHelper {
         return currentElement;
     }
     
+    private List<Element> getElementsByName(Element root, String name) {
+        return root.getChildren(name);
+    }
+    
     /**
      * Check document field, throw exception if field is new (document was neither read
      * nor created)
@@ -90,5 +96,61 @@ public class XMLHelper {
         checkDocument();
         
         XMLFileHelper.writeDocumentToFile(this.document, fileName);
+    }
+    
+    /**
+     * Delete parent of element, it is way to overcomplicated to explain how it works.
+     * I will redesign it later.
+     * 
+     * @param path
+     * @param parentName
+     * @param queryName
+     * @param queryValue
+     * @throws XMLerException 
+     */
+    public void deleteParentOfElement(String path, String parentName, String queryName, String queryValue) throws XMLerException {
+        checkDocument();
+        
+        Element root = getElement(getRoot(), path);
+        
+        List<Element> elements = getElementsByName(root, parentName);
+        
+        elements.removeIf(el -> el.getChild(queryName).getText().equals(queryValue));
+    }
+    
+    public List findElements(String path, String parentName, String queryName, String queryValue) throws XMLHelperDocumentNotExistsException {
+        checkDocument();
+        
+        Element root = getElement(getRoot(), path);
+        
+        List<Element> elements = getElementsByName(root, parentName);
+
+        List<Element> found = elements.stream()
+            .filter(el -> el.getChild(queryName).getText().equals(queryValue))
+            .collect(Collectors.toList());
+        
+        return found;
+    }
+    
+    public boolean checkElementExistence(String path, String parentName, String queryName, String queryValue) throws XMLHelperDocumentNotExistsException {
+        List<Element> found = findElements(path, parentName, queryName, queryValue);
+        
+        return !found.isEmpty();
+    }
+    
+    public List getAll(String path) throws XMLHelperDocumentNotExistsException, Exception {
+        checkDocument();
+        
+        Element root = getElement(getRoot(), path);
+        
+        return root.getChildren();
+    }
+    
+    public List get(String path) throws XMLHelperDocumentNotExistsException {
+        checkDocument();
+        
+        Element root = getElement(getRoot(), path);
+                
+        return root.getChildren();
     }
 }
